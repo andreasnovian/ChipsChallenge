74c9b30a55f632de5d3606a8800d40bb801bbcc3
andreasnovian <andreas.novian@yahoo.com>
2014-10-28 09:58:22 +0700

    Nambahin kelas PanelTester
    Buat nyoba ngeload gambar ke Panel trus nampilin lewat JFrame kayak di
    praktikum GrafKom

---
+++ src/GUI/PanelTester.java
@@ -0,0 +1,116 @@
+
+package GUI;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+/**
+ *
+ * @author Andreas Novian
+ */
+public class PanelTester extends JPanel {
+    public static final int CANVAS_WIDTH = 750;
+    public static final int CANVAS_HEIGHT = 750;
+    public static final String TITLE = "Coba Masukkin Gambar";
+    
+    private Image[][] img;
+    
+    public PanelTester(){
+        setImage();
+        setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
+    }
+    
+    public void setImage(){
+        img  = new Image[15][15];
+        
+        //Load image untuk Brian
+        URL[] brianUrl = new URL[4];
+        brianUrl[0] = getClass().getClassLoader().getResource("GUI/Characters/brianUp.png");
+        brianUrl[1] = getClass().getClassLoader().getResource("GUI/Characters/brianDown.png");
+        brianUrl[2] = getClass().getClassLoader().getResource("GUI/Characters/brianRight.png");
+        brianUrl[3] = getClass().getClassLoader().getResource("GUI/Characters/brianLeft.png");
+        
+        //Load image untuk Candra
+        URL[] candraUrl = new URL[4];
+        candraUrl[0] = getClass().getClassLoader().getResource("GUI/Characters/candraDown.png");
+        candraUrl[1] = getClass().getClassLoader().getResource("GUI/Characters/candraDown.png");
+        candraUrl[2] = getClass().getClassLoader().getResource("GUI/Characters/candraRight.png");
+        candraUrl[3] = getClass().getClassLoader().getResource("GUI/Characters/candraLeft.png");
+        
+        //Load image untuk mantan
+        URL[] mantanUrl = new URL[4];
+        mantanUrl[0] = getClass().getClassLoader().getResource("GUI/Characters/mantanDown.png");
+        mantanUrl[1] = getClass().getClassLoader().getResource("GUI/Characters/mantanDown.png");
+        mantanUrl[2] = getClass().getClassLoader().getResource("GUI/Characters/mantanRight.png");
+        mantanUrl[3] = getClass().getClassLoader().getResource("GUI/Characters/mantanLeft.png");
+        
+        //Load image untuk Mariska
+        URL[] mariskaUrl = new URL[4];
+        mariskaUrl[0] = getClass().getClassLoader().getResource("GUI/Characters/mariskaDown.png");
+        mariskaUrl[1] = getClass().getClassLoader().getResource("GUI/Characters/mariskaDown.png");
+        mariskaUrl[2] = getClass().getClassLoader().getResource("GUI/Characters/mariskaRight.png");
+        mariskaUrl[3] = getClass().getClassLoader().getResource("GUI/Characters/mariskaLeft.png");
+        
+        //Load image untuk Pascal
+        URL[] pascalUrl = new URL[4];
+        pascalUrl[0] = getClass().getClassLoader().getResource("GUI/Characters/pascalDown.png");
+        pascalUrl[1] = getClass().getClassLoader().getResource("GUI/Characters/pascalDown.png");
+        pascalUrl[2] = getClass().getClassLoader().getResource("GUI/Characters/pascalRight.png");
+        pascalUrl[3] = getClass().getClassLoader().getResource("GUI/Characters/pascalLeft.png");
+        
+        //Proses memasukkan gambar yang sudah diload ke array tile
+        try{
+        for (int i=0;i<15;i++){
+            for (int j=0;j<15;j++){
+                if (i%5==0){
+                    img[i][j] = ImageIO.read(brianUrl[1]);
+                }
+                else if (i%5==1){
+                    img[i][j] = ImageIO.read(brianUrl[1]);
+                }
+                else if (i%5==2){
+                    img[i][j] = ImageIO.read(brianUrl[1]);
+                }
+                else if (i%5==3){
+                    img[i][j] = ImageIO.read(brianUrl[1]);
+                }
+                else {
+                    img[i][j] = ImageIO.read(brianUrl[1]);
+                }
+            }
+        }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        setBackground(Color.WHITE);
+        int x=0;
+        int y=0;
+        for (int i=0;i<15;i++){
+            for (int j=0;j<15;j++){
+                g.drawImage(img[i][j],x,y,null);
+                x+=50;
+            }
+            y+=50;
+            x=0;
+        }
+    }
+    
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Coba");
+        frame.setContentPane(new PanelTester());
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
