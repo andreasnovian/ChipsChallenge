639fe07c90cdf771857e12f32cd087c26bad1ad6
andreasnovian <andreas.novian@yahoo.com>
2014-10-28 10:06:29 +0700

    Ngerapihin kelas PanelTester

--- src/GUI/PanelTester.java
+++ src/GUI/PanelTester.java
@@ -4,8 +4,6 @@ package GUI;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
@@ -16,52 +14,55 @@ import javax.swing.*;
 public class PanelTester extends JPanel {
     public static final int CANVAS_WIDTH = 750;
     public static final int CANVAS_HEIGHT = 750;
-    public static final String TITLE = "Coba Masukkin Gambar";
     
     private Image[][] img;
+    private URL[] brianUrl,candraUrl,mantanUrl,mariskaUrl,pascalUrl;
     
     public PanelTester(){
+        loadImage();
         setImage();
         setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
     }
     
-    public void setImage(){
-        img  = new Image[15][15];
-        
+    private void loadImage(){
         //Load image untuk Brian
-        URL[] brianUrl = new URL[4];
+        brianUrl = new URL[4];
         brianUrl[0] = getClass().getClassLoader().getResource("GUI/Characters/brianUp.png");
         brianUrl[1] = getClass().getClassLoader().getResource("GUI/Characters/brianDown.png");
         brianUrl[2] = getClass().getClassLoader().getResource("GUI/Characters/brianRight.png");
         brianUrl[3] = getClass().getClassLoader().getResource("GUI/Characters/brianLeft.png");
         
         //Load image untuk Candra
-        URL[] candraUrl = new URL[4];
+        candraUrl = new URL[4];
         candraUrl[0] = getClass().getClassLoader().getResource("GUI/Characters/candraDown.png");
         candraUrl[1] = getClass().getClassLoader().getResource("GUI/Characters/candraDown.png");
         candraUrl[2] = getClass().getClassLoader().getResource("GUI/Characters/candraRight.png");
         candraUrl[3] = getClass().getClassLoader().getResource("GUI/Characters/candraLeft.png");
         
         //Load image untuk mantan
-        URL[] mantanUrl = new URL[4];
+        mantanUrl = new URL[4];
         mantanUrl[0] = getClass().getClassLoader().getResource("GUI/Characters/mantanDown.png");
         mantanUrl[1] = getClass().getClassLoader().getResource("GUI/Characters/mantanDown.png");
         mantanUrl[2] = getClass().getClassLoader().getResource("GUI/Characters/mantanRight.png");
         mantanUrl[3] = getClass().getClassLoader().getResource("GUI/Characters/mantanLeft.png");
         
         //Load image untuk Mariska
-        URL[] mariskaUrl = new URL[4];
+        mariskaUrl = new URL[4];
         mariskaUrl[0] = getClass().getClassLoader().getResource("GUI/Characters/mariskaDown.png");
         mariskaUrl[1] = getClass().getClassLoader().getResource("GUI/Characters/mariskaDown.png");
         mariskaUrl[2] = getClass().getClassLoader().getResource("GUI/Characters/mariskaRight.png");
         mariskaUrl[3] = getClass().getClassLoader().getResource("GUI/Characters/mariskaLeft.png");
         
         //Load image untuk Pascal
-        URL[] pascalUrl = new URL[4];
+        pascalUrl = new URL[4];
         pascalUrl[0] = getClass().getClassLoader().getResource("GUI/Characters/pascalDown.png");
         pascalUrl[1] = getClass().getClassLoader().getResource("GUI/Characters/pascalDown.png");
         pascalUrl[2] = getClass().getClassLoader().getResource("GUI/Characters/pascalRight.png");
         pascalUrl[3] = getClass().getClassLoader().getResource("GUI/Characters/pascalLeft.png");
+    }
+    
+    private void setImage(){
+        img  = new Image[15][15];
         
         //Proses memasukkan gambar yang sudah diload ke array tile
         try{
@@ -71,16 +72,16 @@ public class PanelTester extends JPanel {
                     img[i][j] = ImageIO.read(brianUrl[1]);
                 }
                 else if (i%5==1){
-                    img[i][j] = ImageIO.read(brianUrl[1]);
+                    img[i][j] = ImageIO.read(candraUrl[1]);
                 }
                 else if (i%5==2){
-                    img[i][j] = ImageIO.read(brianUrl[1]);
+                    img[i][j] = ImageIO.read(mantanUrl[1]);
                 }
                 else if (i%5==3){
-                    img[i][j] = ImageIO.read(brianUrl[1]);
+                    img[i][j] = ImageIO.read(mariskaUrl[1]);
                 }
                 else {
-                    img[i][j] = ImageIO.read(brianUrl[1]);
+                    img[i][j] = ImageIO.read(pascalUrl[1]);
                 }
             }
         }
