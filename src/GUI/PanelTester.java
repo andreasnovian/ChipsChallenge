9369d25389ea5b4d11765616cbf1d051c345da86
andreasnovian <andreas.novian@yahoo.com>
2014-10-28 10:30:46 +0700

    Nambahin load image buat gambar gambar Rooms
    Termasuk: chair, door, key, pc, script, table, wall.

--- src/GUI/PanelTester.java
+++ src/GUI/PanelTester.java
@@ -16,7 +16,7 @@ public class PanelTester extends JPanel {
     public static final int CANVAS_HEIGHT = 750;
     
     private Image[][] img;
-    private URL[] brianUrl,candraUrl,mantanUrl,mariskaUrl,pascalUrl;
+    private URL[] brianUrl,candraUrl,mantanUrl,mariskaUrl,pascalUrl,chairUrl,doorUrl,keyUrl,pcUrl,scriptUrl,tableUrl,wallUrl;
     
     public PanelTester(){
         loadImage();
@@ -59,6 +59,63 @@ public class PanelTester extends JPanel {
         pascalUrl[1] = getClass().getClassLoader().getResource("GUI/Characters/pascalDown.png");
         pascalUrl[2] = getClass().getClassLoader().getResource("GUI/Characters/pascalRight.png");
         pascalUrl[3] = getClass().getClassLoader().getResource("GUI/Characters/pascalLeft.png");
+        
+        //Load image untuk chair
+        chairUrl = new URL[1];
+        chairUrl[0] = getClass().getClassLoader().getResource("GUI/Rooms/chair.png");
+        
+        //Load image untuk door
+        doorUrl = new URL[14];
+        doorUrl[0] = getClass().getClassLoader().getResource("GUI/Rooms/doorA.png");
+        doorUrl[1] = getClass().getClassLoader().getResource("GUI/Rooms/doorB.png");
+        doorUrl[2] = getClass().getClassLoader().getResource("GUI/Rooms/doorC.png");
+        doorUrl[3] = getClass().getClassLoader().getResource("GUI/Rooms/doorD.png");
+        doorUrl[4] = getClass().getClassLoader().getResource("GUI/Rooms/doorE.png");
+        doorUrl[5] = getClass().getClassLoader().getResource("GUI/Rooms/doorF.png");
+        doorUrl[6] = getClass().getClassLoader().getResource("GUI/Rooms/doorG.png");
+        doorUrl[7] = getClass().getClassLoader().getResource("GUI/Rooms/doorH.png");
+        doorUrl[8] = getClass().getClassLoader().getResource("GUI/Rooms/doorI.png");
+        doorUrl[9] = getClass().getClassLoader().getResource("GUI/Rooms/doorJ.png");
+        doorUrl[10] = getClass().getClassLoader().getResource("GUI/Rooms/doorK.png");
+        doorUrl[11] = getClass().getClassLoader().getResource("GUI/Rooms/doorL.png");
+        doorUrl[12] = getClass().getClassLoader().getResource("GUI/Rooms/doorM.png");
+        doorUrl[13] = getClass().getClassLoader().getResource("GUI/Rooms/doorN.png");
+        
+        //Load image untuk key
+        keyUrl = new URL[14];
+        keyUrl[0] = getClass().getClassLoader().getResource("GUI/Rooms/keyA.png");
+        keyUrl[1] = getClass().getClassLoader().getResource("GUI/Rooms/keyB.png");
+        keyUrl[2] = getClass().getClassLoader().getResource("GUI/Rooms/keyC.png");
+        keyUrl[3] = getClass().getClassLoader().getResource("GUI/Rooms/keyD.png");
+        keyUrl[4] = getClass().getClassLoader().getResource("GUI/Rooms/keyE.png");
+        keyUrl[5] = getClass().getClassLoader().getResource("GUI/Rooms/keyF.png");
+        keyUrl[6] = getClass().getClassLoader().getResource("GUI/Rooms/keyG.png");
+        keyUrl[7] = getClass().getClassLoader().getResource("GUI/Rooms/keyH.png");
+        keyUrl[8] = getClass().getClassLoader().getResource("GUI/Rooms/keyI.png");
+        keyUrl[9] = getClass().getClassLoader().getResource("GUI/Rooms/keyJ.png");
+        keyUrl[10] = getClass().getClassLoader().getResource("GUI/Rooms/keyK.png");
+        keyUrl[11] = getClass().getClassLoader().getResource("GUI/Rooms/keyL.png");
+        keyUrl[12] = getClass().getClassLoader().getResource("GUI/Rooms/keyM.png");
+        keyUrl[13] = getClass().getClassLoader().getResource("GUI/Rooms/keyN.png");
+        
+        //Load image untuk PC
+        pcUrl = new URL[4];
+        pcUrl[0] = getClass().getClassLoader().getResource("GUI/Rooms/pcUp.png");
+        pcUrl[1] = getClass().getClassLoader().getResource("GUI/Rooms/pcDown.png");
+        pcUrl[2] = getClass().getClassLoader().getResource("GUI/Rooms/pcRight.png");
+        pcUrl[3] = getClass().getClassLoader().getResource("GUI/Rooms/pcLeft.png");
+        
+        //Load image untuk script
+        scriptUrl = new URL[1];
+        scriptUrl[0] = getClass().getClassLoader().getResource("GUI/Rooms/script.png");
+        
+        //Load image untuk table
+        tableUrl = new URL[1];
+        tableUrl[0] = getClass().getClassLoader().getResource("GUI/Rooms/table.png");
+        
+        //Load image untuk wall
+        wallUrl = new URL[1];
+        wallUrl[0] = getClass().getClassLoader().getResource("GUI/Rooms/wall.png");
     }
     
     private void setImage(){
@@ -69,19 +126,19 @@ public class PanelTester extends JPanel {
         for (int i=0;i<15;i++){
             for (int j=0;j<15;j++){
                 if (i%5==0){
-                    img[i][j] = ImageIO.read(brianUrl[1]);
+                    img[i][j] = ImageIO.read(tableUrl[0]);
                 }
                 else if (i%5==1){
-                    img[i][j] = ImageIO.read(candraUrl[1]);
+                    img[i][j] = ImageIO.read(wallUrl[0]);
                 }
                 else if (i%5==2){
-                    img[i][j] = ImageIO.read(mantanUrl[1]);
+                    img[i][j] = ImageIO.read(keyUrl[0]);
                 }
                 else if (i%5==3){
-                    img[i][j] = ImageIO.read(mariskaUrl[1]);
+                    img[i][j] = ImageIO.read(pcUrl[0]);
                 }
                 else {
-                    img[i][j] = ImageIO.read(pascalUrl[1]);
+                    img[i][j] = ImageIO.read(scriptUrl[0]);
                 }
             }
         }
